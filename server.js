f25751dea93bd686f7c4aaf4b77ff0f608ca778c
Dinis Neves <dinis.neves@onesource.pt>
2023-06-21 03:14:53 +0100

    Server - Ir buscar mais dados à base de dados

--- server.js
+++ server.js
@@ -4,63 +4,67 @@ const axios = require('axios');
 const app = express();
 const PORT = 3001;
 
-const { Pool } = require('pg');
+const {Pool} = require('pg');
 
 const pool = new Pool({
-    host:"172.17.0.1",
-    port:5432,
-    database:"tpdb",
-    user:"postgres",
-    password:"123"
+    host: "172.17.0.1",
+    port: 5432,
+    database: "tpdb",
+    user: "postgres",
+    password: "123"
 });
-
 
 
 app.use(express.static('client/build'));
 
-app.get('/api/characters', async (req, res) => {
+
+app.get('/api/characters', (req, res) => {
     try {
-        const response = await axios.get('https://the-one-api.dev/v2/character', {
-            headers: {
-                Authorization: 'Bearer qxdqyRuBKfeifCyAd7Ir'
+        pool.query('SELECT * FROM characters', (err, result) => {
+            if (err) {
+                console.error('Error executing query', err);
+                res.status(500).json({error: 'An error occurred'});
+            } else {
+                res.json(result.rows);
             }
         });
-
-        res.json(response.data.docs);
     } catch (error) {
         console.error('Erro ao buscar os personagens:', error);
-        res.status(500).json({ error: 'Erro ao buscar os personagens.' });
+        res.status(500).json({error: 'Erro ao buscar os personagens.'});
     }
 });
 
 
-
 app.get('/api/movies', (req, res) => {
-    pool.query('SELECT * FROM movies', (err, result) => {
-        if (err) {
-            console.error('Error executing query', err);
-            res.status(500).json({ error: 'An error occurred' });
-        } else {
-            res.json(result.rows);
-        }
-    });
+    try {
+        pool.query('SELECT * FROM movies', (err, result) => {
+            if (err) {
+                console.error('Error executing query', err);
+                res.status(500).json({error: 'An error occurred'});
+            } else {
+                res.json(result.rows);
+            }
+        });
+    } catch (error) {
+        console.error('Erro ao buscar os filmes:', error);
+        res.status(500).json({error: 'Erro ao buscar os filmes.'});
+    }
 });
 
 
-
-
-app.get('/api/quotes', async (req, res) => {
+app.get('/api/quotes', (req, res) => {
     try {
-        const response = await axios.get('https://the-one-api.dev/v2/quote', {
-            headers: {
-                Authorization: 'Bearer qxdqyRuBKfeifCyAd7Ir'
+        pool.query('SELECT * FROM quotes', (err, result) => {
+            if (err) {
+                console.error('Error executing query', err);
+                res.status(500).json({error: 'An error occurred'});
+            } else {
+                res.json(result.rows);
             }
         });
-
-        res.json(response.data.docs);
     } catch (error) {
         console.error('Erro ao buscar as citações:', error);
-        res.status(500).json({ error: 'Erro ao buscar as citações.' });
+        res.status(500).json({error: 'Erro ao buscar as citações.'});
     }
 });
 
