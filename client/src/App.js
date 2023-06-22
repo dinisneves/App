c1b2919af2807f9d0b8298c1a58b2c96c4761007
Dinis Neves <dinis.neves@onesource.pt>
2023-06-22 21:00:05 +0100

    Personagens + Filmes + Citações (Put, Post, Delete e Get) -> Tem todas as funcionalidades

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,7 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [formData, setFormData] = useState({
     title: '',
-    runtimeMinutes: '',
-    budget: '',
-    boxOffice: '',
-    awardNominations: '',
-    awardWins: '',
-    rtScore: '',
-    genre: ''
   });
-
 
   useEffect(() => {
     fetchMovies();
@@ -22,7 +14,7 @@ function App() {
 
   const [updateFormData, setUpdateFormData] = useState({
     movieId: '',
-    newTitle: ''
+    newTitle: '',
   });
 
   const fetchMovies = async () => {
@@ -40,28 +32,14 @@ function App() {
 
   const handleCreateMovie = async () => {
     try {
-      const { title, runtimeMinutes, budget, boxOffice, awardNominations, awardWins, rtScore, genre } = formData;
+      const { title } = formData;
 
       await axios.post('/api/movies', {
-        title,
-        runtimeMinutes,
-        budget,
-        boxOffice,
-        awardNominations,
-        awardWins,
-        rtScore,
-        genre
+        name: title,
       });
 
       setFormData({
         title: '',
-        runtimeMinutes: '',
-        budget: '',
-        boxOffice: '',
-        awardNominations: '',
-        awardWins: '',
-        rtScore: '',
-        genre: ''
       });
 
       fetchMovies();
@@ -82,66 +60,335 @@ function App() {
   const handleUpdateTitle = async () => {
     try {
       const { movieId, newTitle } = updateFormData;
-      await axios.put(`/api/movies/${movieId}`, { title: newTitle });
+      await axios.put(`/api/movies/${movieId}`, { name: newTitle });
       setUpdateFormData({ movieId: '', newTitle: '' });
       fetchMovies();
     } catch (error) {
       console.error('Erro ao atualizar o título do filme:', error);
     }
   };
+
+  const [characters, setCharacters] = useState([]);
+  const [characterFormData, setCharacterFormData] = useState({
+    name: '',
+    newCharacterName: '',
+  });
+
+  const fetchCharacters = async () => {
+    try {
+      const response = await axios.get('/api/characters');
+      setCharacters(response.data);
+    } catch (error) {
+      console.error('Erro ao buscar os personagens:', error);
+    }
+  };
+
+  const handleCharacterInputChange = (e) => {
+    setCharacterFormData({ ...characterFormData, [e.target.name]: e.target.value });
+  };
+
+  const handleCreateCharacter = async () => {
+    try {
+      const { name } = characterFormData;
+
+      await axios.post('/api/characters', {
+        name,
+      });
+
+      setCharacterFormData({
+        name: '',
+        newCharacterName: '',
+      });
+
+      fetchCharacters();
+    } catch (error) {
+      console.error('Erro ao criar o personagem:', error);
+    }
+  };
+
+  const handleDeleteCharacter = async (characterId) => {
+    try {
+      await axios.delete(`/api/characters/${characterId}`);
+      fetchCharacters();
+    } catch (error) {
+      console.error('Erro ao excluir o personagem:', error);
+    }
+  };
+
+  const handleUpdateCharacterName = async (characterId) => {
+    try {
+      const { newCharacterName } = characterFormData;
+      await axios.put(`/api/characters/${characterId}`, { name: newCharacterName });
+      setCharacterFormData({ ...characterFormData, newCharacterName: '' });
+      fetchCharacters();
+    } catch (error) {
+      console.error('Erro ao atualizar o nome do personagem:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+
+  const [quotes, setQuotes] = useState([]);
+  const [quoteFormData, setQuoteFormData] = useState({
+    dialog: '',
+    newDialog: '', // Adicionado estado para o novo diálogo
+  });
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  const fetchQuotes = async () => {
+    try {
+      const response = await axios.get('/api/quotes');
+      setQuotes(response.data);
+    } catch (error) {
+      console.error('Erro ao buscar as citações:', error);
+    }
+  };
+
+  const handleQuoteInputChange = (e) => {
+    setQuoteFormData({ ...quoteFormData, [e.target.name]: e.target.value });
+  };
+
+  const handleCreateQuote = async () => {
+    try {
+      const { dialog } = quoteFormData;
+
+      await axios.post('/api/quotes', {
+        dialog,
+      });
+
+      setQuoteFormData({
+        dialog: '',
+      });
+
+      fetchQuotes();
+    } catch (error) {
+      console.error('Erro ao criar a citação:', error);
+    }
+  };
+
+  const handleDeleteQuote = async (quoteId) => {
+    try {
+      await axios.delete(`/api/quotes/${quoteId}`);
+      fetchQuotes();
+    } catch (error) {
+      console.error('Erro ao excluir a citação:', error);
+    }
+  };
+
+  const handleUpdateDialog = async (quoteId) => {
+    try {
+      const { newDialog } = quoteFormData;
+      await axios.put(`/api/quotes/${quoteId}`, { dialog: newDialog });
+      setQuoteFormData({ ...quoteFormData, newDialog: '' });
+      fetchQuotes();
+    } catch (error) {
+      console.error('Erro ao atualizar o diálogo da citação:', error);
+    }
+  };
+
+  const [activeTab, setActiveTab] = useState('movies');
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
       <div className="app">
-        <h1>Senhor dos Aneis</h1>
+        <h1>Senhor dos Anéis</h1>
+        <div className="tabs">
+          <button
+              className={`tab ${activeTab === 'movies' ? 'active' : ''}`}
+              onClick={() => handleTabChange('movies')}
+          >
+            Filmes
+          </button>
+          <button
+              className={`tab ${activeTab === 'characters' ? 'active' : ''}`}
+              onClick={() => handleTabChange('characters')}
+          >
+            Personagens
+          </button>
+          <button
+              className={`tab ${activeTab === 'quotes' ? 'active' : ''}`}
+              onClick={() => handleTabChange('quotes')}
+          >
+            Citações
+          </button>
+        </div>
         <div className="content">
-          <div className="movies-list">
-            <h2>Lista de Filmes</h2>
-            <ul>
-              {movies.map((movie) => (
-                  <li key={movie.movieId}>
-                    <div className="movie-info">
-                      <span className="movie-id">ID: {movie.movieid}</span>
-                      <span className="movie-title">{movie.title}</span>
-                    </div>
-                    <button className="delete-button" onClick={() => handleDeleteMovie(movie.movieid)}>
-                      Excluir
-                    </button>
-                  </li>
-              ))}
-            </ul>
-          </div>
-          <div className="create-movie">
-            <h2>Criar Filme</h2>
-            <input
-                type="text"
-                name="title"
-                placeholder="Título"
-                value={formData.title}
-                onChange={handleInputChange}
-            />
-            <button className="create-button" onClick={handleCreateMovie}>
-              Criar Filme
-            </button>
-          </div>
-          <div className="create-movie">
-            <h2>Editar Filme</h2>
-            <input
-                type="text"
-                name="movieId"
-                placeholder="ID do Filme"
-                value={updateFormData.movieId}
-                onChange={(e) => setUpdateFormData({ ...updateFormData, movieId: e.target.value })}
-            />
-            <input
-                type="text"
-                name="newTitle"
-                placeholder="Novo Título"
-                value={updateFormData.newTitle}
-                onChange={(e) => setUpdateFormData({ ...updateFormData, newTitle: e.target.value })}
-            />
-            <button className="create-button" onClick={handleUpdateTitle}>
-              Editar Filme
-            </button>
-          </div>
+          {activeTab === 'movies' && (
+              <>
+                <div className="movies-list">
+                  <h2>Lista de Filmes</h2>
+                  <ul>
+                    {movies.map((movie) => (
+                        <li key={movie._id}>
+                          <div className="movie-info">
+                            <span className="movie-id">ID: {movie._id}</span>
+                            <span className="movie-title">{movie.name}</span>
+                          </div>
+                          <button className="delete-button" onClick={() => handleDeleteMovie(movie._id)}>
+                            Excluir
+                          </button>
+                        </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="create-movie">
+                  <h2>Criar Filme</h2>
+                  <input
+                      type="text"
+                      name="title"
+                      placeholder="Título"
+                      value={formData.title}
+                      onChange={handleInputChange}
+                  />
+                  <button className="create-button" onClick={handleCreateMovie}>
+                    Criar Filme
+                  </button>
+                </div>
+                <div className="create-movie">
+                  <h2>Editar Filme</h2>
+                  <input
+                      type="text"
+                      name="movieId"
+                      placeholder="ID do Filme"
+                      value={updateFormData.movieId}
+                      onChange={(e) => setUpdateFormData({ ...updateFormData, movieId: e.target.value })}
+                  />
+                  <input
+                      type="text"
+                      name="newTitle"
+                      placeholder="Novo Título"
+                      value={updateFormData.newTitle}
+                      onChange={(e) => setUpdateFormData({ ...updateFormData, newTitle: e.target.value })}
+                  />
+                  <button className="create-button" onClick={handleUpdateTitle}>
+                    Editar Filme
+                  </button>
+                </div>
+              </>
+          )}
+
+          {activeTab === 'characters' && (
+              <>
+                <div className="movies-list">
+                  <h2>Lista de Personagens</h2>
+                  <ul>
+                    {characters.map((character) => (
+                        <li key={character._id}>
+                          <div className="character-info">
+                            <span className="movie-id">ID: {character._id}</span>
+                            <span className="movie-name">{character.name}</span>
+                          </div>
+                          <button
+                              className="delete-button"
+                              onClick={() => handleDeleteCharacter(character._id)}
+                          >
+                            Excluir
+                          </button>
+                        </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="create-movie">
+                  <h2>Criar Personagem</h2>
+                  <input
+                      type="text"
+                      name="name"
+                      placeholder="Nome"
+                      value={characterFormData.name}
+                      onChange={handleCharacterInputChange}
+                  />
+                  <button className="create-button" onClick={handleCreateCharacter}>
+                    Criar Personagem
+                  </button>
+                </div>
+                <div className="create-movie">
+                  <h2>Editar Personagem</h2>
+                  <input
+                      type="text"
+                      name="characterId"
+                      placeholder="ID do Personagem"
+                      value={characterFormData.characterId}
+                      onChange={handleCharacterInputChange}
+                  />
+                  <input
+                      type="text"
+                      name="newCharacterName"
+                      placeholder="Novo Nome"
+                      value={characterFormData.newCharacterName || ''}
+                      onChange={handleCharacterInputChange}
+                  />
+                  <button
+                      className="create-button"
+                      onClick={() => handleUpdateCharacterName(characterFormData.characterId)}
+                  >
+                    Editar Personagem
+                  </button>
+                </div>
+              </>
+          )}
+          {activeTab === 'quotes' && (
+              <>
+                <div className="movies-list">
+                  <h2>Lista de Citações</h2>
+                  <ul>
+                    {quotes.map((quote) => (
+                        <li key={quote._id}>
+                          <div className="movie-info">
+                            <span className="movie-id">ID: {quote._id}</span>
+                            <span className="movie-title">{quote.dialog}</span>
+                          </div>
+                          <button className="delete-button" onClick={() => handleDeleteQuote(quote._id)}>
+                            Excluir
+                          </button>
+                        </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="create-quote">
+                  <h2>Criar Citação</h2>
+                  <input
+                      type="text"
+                      name="dialog"
+                      placeholder="Diálogo"
+                      value={quoteFormData.dialog}
+                      onChange={handleQuoteInputChange}
+                  />
+                  <button className="create-button" onClick={handleCreateQuote}>
+                    Criar Citação
+                  </button>
+                </div>
+                <div className="create-quote">
+                  <h2>Editar Citação</h2>
+                  <input
+                      type="text"
+                      name="quoteId" // Alterado para "quoteId"
+                      placeholder="ID da Citação"
+                      value={quoteFormData.quoteId}
+                      onChange={handleQuoteInputChange}
+                  />
+                  <input
+                      type="text"
+                      name="newDialog"
+                      placeholder="Novo Diálogo"
+                      value={quoteFormData.newDialog || ''}
+                      onChange={handleQuoteInputChange}
+                  />
+                  <button className="create-button" onClick={() => handleUpdateDialog(quoteFormData.quoteId)}>
+                    Editar Citação
+                  </button>
+                </div>
+
+              </>
+          )}
         </div>
       </div>
   );
--- server.js
+++ server.js
@@ -17,10 +17,9 @@ const pool = new Pool({
 });
 
 // Middlewares
-app.use(express.static('client/build'));
 app.use(bodyParser.json());
 
-// Rotas
+// Rotas API
 app.get('/api/movies', (req, res) => {
     pool.query('SELECT * FROM movies', (err, result) => {
         if (err) {
@@ -33,12 +32,12 @@ app.get('/api/movies', (req, res) => {
 });
 
 app.post('/api/movies', (req, res) => {
-    const { title } = req.body;
-    const movieid = uuidv4(); // Gera um ID único usando o pacote uuid
+    const { name } = req.body;
+    const movieId = uuidv4(); // Gera um ID único usando o pacote uuid
 
     pool.query(
-        'INSERT INTO movies (title, movieid) VALUES ($1, $2)',
-        [title, movieid],
+        'INSERT INTO movies (_id, name) VALUES ($1, $2)',
+        [movieId, name],
         (err, result) => {
             if (err) {
                 console.error('Error executing query', err);
@@ -50,13 +49,13 @@ app.post('/api/movies', (req, res) => {
     );
 });
 
-app.put('/api/movies/:movieId', (req, res) => {
-    const { movieId } = req.params;
-    const { title } = req.body;
+app.put('/api/movies/:_id', (req, res) => {
+    const { _id } = req.params;
+    const { name } = req.body;
 
     pool.query(
-        'UPDATE movies SET title = $1 WHERE movieid = $2',
-        [title, movieId],
+        'UPDATE movies SET name = $1 WHERE _id = $2',
+        [name, _id],
         (err, result) => {
             if (err) {
                 console.error('Error executing query', err);
@@ -69,21 +68,151 @@ app.put('/api/movies/:movieId', (req, res) => {
 });
 
 
-app.delete('/api/movies/:movieId', (req, res) => {
-    const { movieId } = req.params;
+app.delete('/api/movies/:_id', (req, res) => {
+    const { _id } = req.params;
 
-    pool.query('DELETE FROM movies WHERE movieid = $1', [movieId], (err, result) => {
+    pool.query('DELETE FROM movies WHERE _id = $1', [_id], (err, result) => {
         if (err) {
             console.error('Error executing query', err);
             res.status(500).json({ error: 'An error occurred' });
         } else {
-            res.json({ message: `Filme com ID ${movieId} excluído com sucesso` });
+            res.json({ message: `Filme com ID ${_id} excluído com sucesso` });
         }
     });
+});
+
+// Rotas para personagens
+app.get('/api/characters', (req, res) => {
+    pool.query('SELECT * FROM characters', (err, result) => {
+        if (err) {
+            console.error('Error executing query', err);
+            res.status(500).json({ error: 'An error occurred' });
+        } else {
+            res.json(result.rows);
+        }
+    });
+});
+
+app.post('/api/characters', (req, res) => {
+    const { name } = req.body;
+    const characterId = uuidv4(); // Gera um ID único usando o pacote uuid
+
+    pool.query(
+        'INSERT INTO characters (_id, name) VALUES ($1, $2)',
+        [characterId, name],
+        (err, result) => {
+            if (err) {
+                console.error('Error executing query', err);
+                res.status(500).json({ error: 'An error occurred' });
+            } else {
+                res.json({ message: 'Personagem criado com sucesso' });
+            }
+        }
+    );
+});
+
+app.put('/api/characters/:_id', (req, res) => {
+    const { _id } = req.params;
+    const { name } = req.body;
+
+    pool.query(
+        'UPDATE characters SET name = $1 WHERE _id = $2',
+        [name, _id],
+        (err, result) => {
+            if (err) {
+                console.error('Error executing query', err);
+                res.status(500).json({ error: 'An error occurred' });
+            } else {
+                res.json({ message: 'Personagem atualizado com sucesso' });
+            }
+        }
+    );
+});
+
+app.delete('/api/characters/:_id', (req, res) => {
+    const { _id } = req.params;
+
+    pool.query('DELETE FROM characters WHERE _id = $1', [_id], (err, result) => {
+        if (err) {
+            console.error('Error executing query', err);
+            res.status(500).json({ error: 'An error occurred' });
+        } else {
+            res.json({ message: `Personagem com ID ${_id} excluído com sucesso` });
+        }
+    });
+});
+
+// Rotas para quotes
+app.get('/api/quotes', (req, res) => {
+    pool.query('SELECT * FROM quotes', (err, result) => {
+        if (err) {
+            console.error('Error executing query', err);
+            res.status(500).json({ error: 'An error occurred' });
+        } else {
+            res.json(result.rows);
+        }
+    });
+});
+
+app.post('/api/quotes', (req, res) => {
+    const { dialog } = req.body;
+    const quoteId = uuidv4(); // Gera um ID único usando o pacote uuid
+
+    pool.query(
+        'INSERT INTO quotes (_id, dialog) VALUES ($1, $2)',
+        [quoteId, dialog],
+        (err, result) => {
+            if (err) {
+                console.error('Error executing query', err);
+                res.status(500).json({ error: 'An error occurred' });
+            } else {
+                res.json({ message: 'Citação criada com sucesso' });
+            }
+        }
+    );
+});
+
+
+
+app.put('/api/quotes/:_id', (req, res) => {
+    const { _id } = req.params;
+    const { dialog } = req.body;
+
+    pool.query(
+        'UPDATE quotes SET dialog = $1 WHERE _id = $2',
+        [dialog, _id],
+        (err, result) => {
+            if (err) {
+                console.error('Error executing query', err);
+                res.status(500).json({ error: 'An error occurred' });
+            } else {
+                res.json({ message: 'Citação atualizada com sucesso' });
+            }
+        }
+    );
+});
+
+app.delete('/api/quotes/:_id', (req, res) => {
+    const { _id } = req.params;
+
+    pool.query('DELETE FROM quotes WHERE _id = $1', [_id], (err, result) => {
+        if (err) {
+            console.error('Error executing query', err);
+            res.status(500).json({ error: 'An error occurred' });
+        } else {
+            res.json({ message: `Citação com ID ${_id} excluído com sucesso` });
+        }
+    });
+});
+
+// Servir o aplicativo React
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}!`);
 });
- 
