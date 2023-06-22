ecb2820deb7b1b9770d5dc5e17d6b527f583b70d
Dinis Neves <dinis.neves@onesource.pt>
2023-06-22 04:31:34 +0100

    PUT + POST + DELETE + GET dos movies e novo layout

--- client/src/App.js
+++ client/src/App.js
@@ -3,100 +3,145 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [characters, setCharacters] = useState([]);
   const [movies, setMovies] = useState([]);
-  const [quotes, setQuotes] = useState([]);
-  const [activeTab, setActiveTab] = useState('characters');
+  const [formData, setFormData] = useState({
+    title: '',
+    runtimeMinutes: '',
+    budget: '',
+    boxOffice: '',
+    awardNominations: '',
+    awardWins: '',
+    rtScore: '',
+    genre: ''
+  });
+
 
   useEffect(() => {
-    const fetchCharacters = async () => {
-      try {
-        const response = await axios.get('/api/characters');
-        setCharacters(response.data);
-      } catch (error) {
-        console.error('Erro ao buscar os personagens:', error);
-      }
-    };
-
-    const fetchMovies = async () => {
-      try {
-        const response = await axios.get('/api/movies');
-        setMovies(response.data);
-      } catch (error) {
-        console.error('Erro ao buscar os filmes:', error);
-      }
-    };
-
-    const fetchQuotes = async () => {
-      try {
-        const response = await axios.get('/api/quotes');
-        setQuotes(response.data);
-      } catch (error) {
-        console.error('Erro ao buscar as citações:', error);
-      }
-    };
-
-    fetchCharacters();
     fetchMovies();
-    fetchQuotes();
   }, []);
 
-  const getMovieName = (quote) => {
-    const movie = movies.find((m) => m._id === quote.movie);
-    return movie ? movie.name : '';
+  const [updateFormData, setUpdateFormData] = useState({
+    movieId: '',
+    newTitle: ''
+  });
+
+  const fetchMovies = async () => {
+    try {
+      const response = await axios.get('/api/movies');
+      setMovies(response.data);
+    } catch (error) {
+      console.error('Erro ao buscar os filmes:', error);
+    }
   };
 
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleCreateMovie = async () => {
+    try {
+      const { title, runtimeMinutes, budget, boxOffice, awardNominations, awardWins, rtScore, genre } = formData;
+
+      await axios.post('/api/movies', {
+        title,
+        runtimeMinutes,
+        budget,
+        boxOffice,
+        awardNominations,
+        awardWins,
+        rtScore,
+        genre
+      });
+
+      setFormData({
+        title: '',
+        runtimeMinutes: '',
+        budget: '',
+        boxOffice: '',
+        awardNominations: '',
+        awardWins: '',
+        rtScore: '',
+        genre: ''
+      });
+
+      fetchMovies();
+    } catch (error) {
+      console.error('Erro ao criar o filme:', error);
+    }
+  };
+
+  const handleDeleteMovie = async (movieId) => {
+    try {
+      await axios.delete(`/api/movies/${movieId}`);
+      fetchMovies();
+    } catch (error) {
+      console.error('Erro ao excluir o filme:', error);
+    }
+  };
+
+  const handleUpdateTitle = async () => {
+    try {
+      const { movieId, newTitle } = updateFormData;
+      await axios.put(`/api/movies/${movieId}`, { title: newTitle });
+      setUpdateFormData({ movieId: '', newTitle: '' });
+      fetchMovies();
+    } catch (error) {
+      console.error('Erro ao atualizar o título do filme:', error);
+    }
+  };
   return (
       <div className="app">
-        <h1>O Senhor dos Anéis</h1>
-        <div className="tabs">
-          <div
-              className={`tab ${activeTab === 'characters' ? 'active' : ''}`}
-              onClick={() => setActiveTab('characters')}
-          >
-            Personagens
-          </div>
-          <div
-              className={`tab ${activeTab === 'movies' ? 'active' : ''}`}
-              onClick={() => setActiveTab('movies')}
-          >
-            Filmes
-          </div>
-          <div
-              className={`tab ${activeTab === 'quotes' ? 'active' : ''}`}
-              onClick={() => setActiveTab('quotes')}
-          >
-            Citações
-          </div>
-        </div>
+        <h1>Senhor dos Aneis</h1>
         <div className="content">
-          {activeTab === 'characters' && (
-              <ul>
-                {characters.map((character) => (
-                    <li key={character._id}>
-                      {character.name} - {character.race}
-                    </li>
-                ))}
-              </ul>
-          )}
-          {activeTab === 'movies' && (
-              <ul>
-                {movies.map((movie) => (
-                    <li key={movie._id}>
-                      {movie.title}
-                    </li>
-                ))}
-              </ul>
-          )}
-          {activeTab === 'quotes' && (
-              <ul>
-                {quotes.map((quote) => (
-                    <li key={quote._id}>
-                      {quote.dialog} - {getMovieName(quote)}
-                    </li>
-                ))}
-              </ul>
-          )}
+          <div className="movies-list">
+            <h2>Lista de Filmes</h2>
+            <ul>
+              {movies.map((movie) => (
+                  <li key={movie.movieId}>
+                    <div className="movie-info">
+                      <span className="movie-id">ID: {movie.movieid}</span>
+                      <span className="movie-title">{movie.title}</span>
+                    </div>
+                    <button className="delete-button" onClick={() => handleDeleteMovie(movie.movieid)}>
+                      Excluir
+                    </button>
+                  </li>
+              ))}
+            </ul>
+          </div>
+          <div className="create-movie">
+            <h2>Criar Filme</h2>
+            <input
+                type="text"
+                name="title"
+                placeholder="Título"
+                value={formData.title}
+                onChange={handleInputChange}
+            />
+            <button className="create-button" onClick={handleCreateMovie}>
+              Criar Filme
+            </button>
+          </div>
+          <div className="create-movie">
+            <h2>Editar Filme</h2>
+            <input
+                type="text"
+                name="movieId"
+                placeholder="ID do Filme"
+                value={updateFormData.movieId}
+                onChange={(e) => setUpdateFormData({ ...updateFormData, movieId: e.target.value })}
+            />
+            <input
+                type="text"
+                name="newTitle"
+                placeholder="Novo Título"
+                value={updateFormData.newTitle}
+                onChange={(e) => setUpdateFormData({ ...updateFormData, newTitle: e.target.value })}
+            />
+            <button className="create-button" onClick={handleUpdateTitle}>
+              Editar Filme
+            </button>
+          </div>
         </div>
       </div>
   );
--- server.js
+++ server.js
@@ -1,77 +1,89 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+
 const app = express();
 const PORT = 3001;
+const { v4: uuidv4 } = require('uuid');
 
-const {Pool} = require('pg');
-
+// Configuração do banco de dados
 const pool = new Pool({
-    host: "172.17.0.1",
+    host: 'localhost',
     port: 5432,
-    database: "tpdb",
-    user: "postgres",
-    password: "123"
+    database: 'tpdb',
+    user: 'postgres',
+    password: '123'
 });
 
-
+// Middlewares
 app.use(express.static('client/build'));
+app.use(bodyParser.json());
 
-
-app.get('/api/characters', (req, res) => {
-    try {
-        pool.query('SELECT * FROM characters', (err, result) => {
-            if (err) {
-                console.error('Error executing query', err);
-                res.status(500).json({error: 'An error occurred'});
-            } else {
-                res.json(result.rows);
-            }
-        });
-    } catch (error) {
-        console.error('Erro ao buscar os personagens:', error);
-        res.status(500).json({error: 'Erro ao buscar os personagens.'});
-    }
-});
-
-
+// Rotas
 app.get('/api/movies', (req, res) => {
-    try {
-        pool.query('SELECT * FROM movies', (err, result) => {
+    pool.query('SELECT * FROM movies', (err, result) => {
+        if (err) {
+            console.error('Error executing query', err);
+            res.status(500).json({ error: 'An error occurred' });
+        } else {
+            res.json(result.rows);
+        }
+    });
+});
+
+app.post('/api/movies', (req, res) => {
+    const { title } = req.body;
+    const movieid = uuidv4(); // Gera um ID único usando o pacote uuid
+
+    pool.query(
+        'INSERT INTO movies (title, movieid) VALUES ($1, $2)',
+        [title, movieid],
+        (err, result) => {
             if (err) {
                 console.error('Error executing query', err);
-                res.status(500).json({error: 'An error occurred'});
+                res.status(500).json({ error: 'An error occurred' });
             } else {
-                res.json(result.rows);
+                res.json({ message: 'Filme criado com sucesso' });
             }
-        });
-    } catch (error) {
-        console.error('Erro ao buscar os filmes:', error);
-        res.status(500).json({error: 'Erro ao buscar os filmes.'});
-    }
+        }
+    );
 });
 
+app.put('/api/movies/:movieId', (req, res) => {
+    const { movieId } = req.params;
+    const { title } = req.body;
 
-app.get('/api/quotes', (req, res) => {
-    try {
-        pool.query('SELECT * FROM quotes', (err, result) => {
+    pool.query(
+        'UPDATE movies SET title = $1 WHERE movieid = $2',
+        [title, movieId],
+        (err, result) => {
             if (err) {
                 console.error('Error executing query', err);
-                res.status(500).json({error: 'An error occurred'});
+                res.status(500).json({ error: 'An error occurred' });
             } else {
-                res.json(result.rows);
+                res.json({ message: 'Filme atualizado com sucesso' });
             }
-        });
-    } catch (error) {
-        console.error('Erro ao buscar as citações:', error);
-        res.status(500).json({error: 'Erro ao buscar as citações.'});
-    }
+        }
+    );
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+
+app.delete('/api/movies/:movieId', (req, res) => {
+    const { movieId } = req.params;
+
+    pool.query('DELETE FROM movies WHERE movieid = $1', [movieId], (err, result) => {
+        if (err) {
+            console.error('Error executing query', err);
+            res.status(500).json({ error: 'An error occurred' });
+        } else {
+            res.json({ message: `Filme com ID ${movieId} excluído com sucesso` });
+        }
+    });
 });
 
+// Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}!`);
 });
+ 
